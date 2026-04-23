af9c2257cae36676009db22bce3d240ce26e9ba3
NekoKaicho <adecta7@gmail.com>
2026-04-23 12:21:25 +0700

    Menambah fungsi addNode, bagian input NIM

--- SingleLinkedList.cpp
+++ SingleLinkedList.cpp
@@ -17,3 +17,14 @@ public:
     {
         START = NULL;
     }
+
+    void addNode()
+    {
+        int nim;
+        cout << "\nMasukkan Nomor Mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node();
+        nodeBaru->naMhs = nim;
+
+       
